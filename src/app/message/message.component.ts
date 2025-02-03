a3763b6039bb163f96b8d324863a121a231a634a
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-03 09:52:44 -0300

    Created messages route

---
+++ src/app/message/message.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+
+@Component({
+  selector: 'app-message',
+  standalone: true,
+  imports: [],
+  templateUrl: './message.component.html',
+  styleUrl: './message.component.css'
+})
+export class MessageComponent {
+  chats: Array<any> = [];
+
+  constructor(private apiService: ApiService) {
+    this.apiService.getChats("gabrielf.04.2002@springmail.com").subscribe({
+      next: response => {
+        console.log(response);
+        this.chats = response;
+      }
+    })
+  }
+
+}
