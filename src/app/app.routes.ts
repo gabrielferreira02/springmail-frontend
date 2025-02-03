c4d8a3da39a5e7be27f23e3ced01450d4d26cc91
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-03 09:52:16 -0300

    Update - Added routes for users homepage

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,17 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { BoxComponent } from './box/box.component';
+import { MessageComponent } from './message/message.component';
 
 export const routes: Routes = [
     {path: "", component: LoginComponent},
-    {path: "register", component: RegisterComponent}
+    {path: "register", component: RegisterComponent},
+    {
+        path: "box", 
+        component: BoxComponent,
+        children: [
+            {path: "", component: MessageComponent}
+        ]
+    }
 ];
