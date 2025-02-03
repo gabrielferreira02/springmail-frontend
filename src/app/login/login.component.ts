0ad0f9f3cfb2225a8f52446aa44ef5c1af1f1bfc
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-03 09:54:21 -0300

    Added a test method to form login in main class to redirect to box endpoint

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,13 +1,20 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgForm } from '@angular/forms';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, FormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  onSubmit(login: NgForm) {
+    this.router.navigate(["/box"]);
+  }
 }
