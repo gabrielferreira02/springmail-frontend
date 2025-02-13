e9a8deeb14e774340e302798cc4e047e95bda201
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-13 13:31:35 -0300

    Created sent component

---
+++ src/app/sent/sent.component.ts
@@ -0,0 +1,105 @@
+import { Component } from '@angular/core';
+import { ApiService } from '../api.service';
+import { Router, RouterLink } from '@angular/router';
+import { UserService } from '../user.service';
+
+@Component({
+  selector: 'app-sent',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './sent.component.html',
+  styleUrl: './sent.component.css'
+})
+export class SentComponent {
+  chats: Array<any> = [];
+
+  constructor(private apiService: ApiService, 
+              private userService: UserService,
+              private router: Router) {
+    this.apiService.getSentChats(this.userService.getEmail()).subscribe({
+      next: response => {
+        console.log(response);
+        this.chats = response;
+      },
+      error: response => {
+        if(response.error.status == 403) {
+          this.router.navigate([""]);
+        }
+      }
+    })
+  }
+
+  reload() {
+    this.apiService.getSentChats(this.userService.getEmail()).subscribe({
+      next: response => {
+        console.log(response);
+        this.chats = response;
+      },
+      error: response => {
+        if(response.error.status == 403) {
+          this.router.navigate([""]);
+        }
+      }
+    })
+  }
+
+  onChange(username: string) {
+    if(username === "") return;
+
+    setTimeout(() => {
+      this.apiService.getChatsToByUsername(username, this.userService.getEmail()).subscribe({
+        next: response => {
+          this.chats = response;
+        },
+        error: response => {
+          if(response.error.status == 403) {
+            this.router.navigate([""]);
+          }
+        }
+      })
+    }, 1000)
+  }
+
+  getDate(date: string): string {
+    const d = new Date(date);
+    
+    const now = new Date();
+    if(d.getDay == now.getDay) {
+      const hour = d.toLocaleString("pt-br", {timeZone: "America/Sao_Paulo"})
+      return `${hour.substring(11, 17)}`;
+    }
+
+    return `${d.getDay} ${d.toLocaleString("pt-br", {month: "short"})}`
+  }
+
+  favorite(item: any) {
+    const data = {
+      chatId: item.id,
+      userEmail: this.userService.getEmail()
+    };
+
+    if(!item.isFavorite) {
+      this.apiService.favoriteChat(data).subscribe({
+        next: response => {
+          item.isFavorite = true;
+        },
+        error: response => {
+          if(response.error.status == 403) {
+            this.router.navigate([""]);
+          }
+        }
+      })
+    } else {
+      this.apiService.deleteFavoriteChat(data).subscribe({
+        next: response => {
+          item.isFavorite = false;
+        },
+        error: response => {
+          if(response.error.status == 403) {
+            this.router.navigate([""]);
+          }
+        }
+      })
+    }
+  }
+}
