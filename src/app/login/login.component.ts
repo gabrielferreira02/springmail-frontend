e9a918e7606e19e2975a01e7dd92e9e14f7033c4
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-13 13:34:17 -0300

    Update - Added form validation in ts file -  Updated html file

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,18 +3,49 @@ import { RouterLink } from '@angular/router';
 import { NgForm } from '@angular/forms';
 import { FormsModule } from '@angular/forms';
 import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [RouterLink, FormsModule],
-  templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  templateUrl: './login.component.html'
 })
 export class LoginComponent {
-  constructor(private router: Router) {}
+  constructor(private router: Router, private apiService: ApiService, private userService: UserService) {}
 
   onSubmit(login: NgForm) {
-    this.router.navigate(["/box"]);
+    const p = document.getElementById("error");
+
+    p!.innerHTML = "";
+    if(login.valid) {
+      if(login.value.mail === "") {
+        p!.innerHTML = "Campo email não pode ser vazio";
+        return;
+      }
+
+      if(login.value.password.length < 8) {
+        p!.innerHTML = "Campo senha precisa ter ao menos 8 digitos";
+        return;
+      }
+
+      const data = {
+        email: login.value.mail,
+        password: login.value.password
+      }
+
+      this.apiService.login(data).subscribe({
+        next: response => {
+          this.userService.setEmail(response.email);
+          this.userService.setToken(response.token);
+          console.log(response.token)
+          this.router.navigate(["/box/messages"]);
+        }, 
+        error: e => {
+          p!.innerHTML = "Email ou senha incorretos";
+        }
+      })
+    }
   }
 }
