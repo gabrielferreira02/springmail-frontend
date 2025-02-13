b08dc16c6ae899c5132a8468a6e02dc424a80fde
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-13 13:28:16 -0300

    Added guard to routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,6 +5,9 @@ import { BoxComponent } from './box/box.component';
 import { MessageComponent } from './message/message.component';
 import { SentComponent } from './sent/sent.component';
 import { FavoriteComponent } from './favorite/favorite.component';
+import { AccountComponent } from './account/account.component';
+import { ChatComponent } from './chat/chat.component';
+import { authGuard } from './auth.guard';
 
 export const routes: Routes = [
     {path: "", component: LoginComponent},
@@ -12,10 +15,13 @@ export const routes: Routes = [
     {
         path: "box", 
         component: BoxComponent,
+        canActivate: [authGuard],
         children: [
-            {path: "messages", component: MessageComponent},
-            {path: "sent", component: SentComponent},
-            {path: "favorites", component: FavoriteComponent}
+            {path: "messages", component: MessageComponent, canActivate: [authGuard]},
+            {path: "sent", component: SentComponent, canActivate: [authGuard]},
+            {path: "favorites", component: FavoriteComponent, canActivate: [authGuard]},
+            {path: "account",  component: AccountComponent, canActivate: [authGuard]},
+            {path: "messages/:id", component: ChatComponent, canActivate: [authGuard]}
         ]
     }
 ];
