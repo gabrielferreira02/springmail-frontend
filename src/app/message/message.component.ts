fdb6728f439593bf101d8ac70b649b21ccaf6388
Gabriel Ferreira <gabrielf.04.2002@gmail.com>
2025-02-14 19:22:21 -0300

    Added content to html and implemented methods in ts file

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,22 +1,132 @@
 import { Component } from '@angular/core';
 import { ApiService } from '../api.service';
+import { UserService } from '../user.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-message',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './message.component.html'
 })
 export class MessageComponent {
   chats: Array<any> = [];
 
-  constructor(private apiService: ApiService) {
-    this.apiService.getChats("gabrielf.04.2002@springmail.com").subscribe({
+  constructor(private apiService: ApiService, private userService: UserService, private router: Router) {
+    this.apiService.getChats(this.userService.getEmail()).subscribe({
       next: response => {
         console.log(response);
         this.chats = response;
       }
     })
+  }
+
+  onChange(username: string) {
+    if(username === "") return;
+
+    setTimeout(() => {
+      this.apiService.getChatsFromByUsername(username, this.userService.getEmail()).subscribe({
+        next: response => {
+          this.chats = response;
+        },
+        error: response => {
+          if(response.error.status == 403) {
+            this.router.navigate([""]);
+          }
+        }
+      })
+    }, 1000)
+  }
+
+  favorite(item: any) {
+    const data = {
+      chatId: item.id,
+      userEmail: this.userService.getEmail()
+    };
+
+    if(!item.isFavorite) {
+      this.apiService.favoriteChat(data).subscribe({
+        next: response => {
+          item.isFavorite = true;
+        },
+        error: response => {
+          if(response.error.status == 403) {
+            this.router.navigate([""]);
+          }
+        }
+      })
+    } else {
+      this.apiService.deleteFavoriteChat(data).subscribe({
+        next: response => {
+          item.isFavorite = false;
+        },
+        error: response => {
+          if(response.error.status == 403) {
+            this.router.navigate([""]);
+          }
+        }
+      })
+    }
+  }
+
+  reload() {
+    this.apiService.getChats(this.userService.getEmail()).subscribe({
+      next: response => {
+        console.log(response);
+        this.chats = response;
+      },
+      error: response => {
+        if(response.error.status == 403) {
+          this.router.navigate([""]);
+        }
+      }
+    })
+  }
+
+  getDate(date: string): string {
+    const d = new Date(date);
+    
+    const now = new Date();
+    if(d.getDay == now.getDay) {
+      const hour = d.toLocaleString("pt-br", {timeZone: "America/Sao_Paulo"})
+      return `${hour.substring(11, 17)}`;
+    }
+
+    return `${d.getDay} ${d.toLocaleString("pt-br", {month: "short"})}`
+  }
+
+  setColor(c: string) {
+    const letterColors: { [key: string]: string } = {
+      A: "bg-red-500",
+      B: "bg-blue-500",
+      C: "bg-green-500",
+      D: "bg-yellow-500",
+      E: "bg-purple-500",
+      F: "bg-pink-500",
+      G: "bg-indigo-500",
+      H: "bg-teal-500",
+      I: "bg-orange-500",
+      J: "bg-gray-500",
+      K: "bg-lime-500",
+      L: "bg-cyan-500",
+      M: "bg-rose-500",
+      N: "bg-violet-500",
+      O: "bg-emerald-500",
+      P: "bg-amber-500",
+      Q: "bg-fuchsia-500",
+      R: "bg-sky-500",
+      S: "bg-lime-700",
+      T: "bg-cyan-700",
+      U: "bg-red-700",
+      V: "bg-blue-700",
+      W: "bg-green-700",
+      X: "bg-yellow-700",
+      Y: "bg-purple-700",
+      Z: "bg-pink-700"
+    };
+
+    return letterColors[c];
+    
   }
 
 }
